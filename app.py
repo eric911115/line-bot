fe98b3ea6724019550afb5ee1dc903cd1399a242
eric wei <aeric911115@gmail.com>
2020-10-17 20:31:39 +0800

    update

--- app.py
+++ app.py
@@ -37,9 +37,16 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    msg = event.message.text
+    r = '我想上大學'
+
+    if msg == '關於瑋彥'
+        r = '小可愛'
+    elif msg == '為什麼要瑋彥'
+        r = '太可愛'
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text=))
 
 
 if __name__ == "__main__":
