ec6b1ead2b6e0228dbccd896921eb93d429a97da
eric wei <aeric911115@gmail.com>
2020-10-17 18:19:08 +0800

    secret

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from linebot.models import (
 app = Flask(__name__)
 
 line_bot_api = LineBotApi('MUwI3W4RRCYR8Ne1+ed8nkgPoS94ctbPsiEyxv4zhgibDNOCCgILzM9f+0AKVe3XAdmrEc3nKYU5nbrP1WZbtbB3H88oNRTToOntthD5uRYBDe6Ry0+PU6l/gKiiIR9t/Aic2e1ipyG0S/NXqGzH0wdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('38419a9be158b9c5a6c062fd386ebab6')
+handler = WebhookHandler('99fceeadf48dbe1ab2cf4325896cee4a')
 
 
 @app.route("/callback", methods=['POST'])
