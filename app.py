58695850b81c22eaab133f3fc7a3d1383f129817
eric wei <aeric911115@gmail.com>
2020-10-17 22:00:24 +0800

    utl

--- app.py
+++ app.py
@@ -49,55 +49,55 @@ def handle_message(event):
     elif msg == '為什麼要瑋彥':
         r = '太可愛'
     elif msg == "關於瑋彥":
-                    line_bot_api.reply_message(  # 回復傳入的訊息文字
-                        event.reply_token,
-                        TemplateSendMessage(
-                            alt_text='Buttons template',
-                            template=ButtonsTemplate(
-                                title='瑋彥的作品集',
-                                text='請選擇作品',
-                                actions=[
-                                    MessageTemplateAction(
-                                        label='IG濾鏡',
-                                        text='IG濾鏡'
-                                    ),
-                                    MessageTemplateAction(
-                                        label='youtube',
-                                        text='youtube'
-                                    ),
-                                    MessageTemplateAction(
-                                        label='網頁',
-                                        text='網頁'
-                                    )
-                                ]
-                            )
+                line_bot_api.reply_message(  # 回復傳入的訊息文字
+                    event.reply_token,
+                    TemplateSendMessage(
+                        alt_text='Buttons template',
+                        template=ButtonsTemplate(
+                            title='瑋彥的作品集',
+                            text='請選擇作品',
+                            actions=[
+                                MessageTemplateAction(
+                                    label='IG濾鏡',
+                                    text='IG濾鏡'
+                                ),
+                                MessageTemplateAction(
+                                    label='youtube',
+                                    text='youtube'
+                                ),
+                                MessageTemplateAction(
+                                    label='網頁',
+                                    text='網頁'
+                                )
+                            ]
                         )
                     )
+                )
     elif msg == "IG濾鏡":
                     line_bot_api.reply_message(  # 回復傳入的訊息文字
-                        event.reply_token,
-                        TemplateSendMessage(
-                            alt_text='Buttons template',
-                            template=ButtonsTemplate(
-                                title='IG濾鏡',
-                                text='請選擇濾鏡，有些濾鏡點擊會有變化',
-                                actions=[
-                                    URIAction(
-                                        label='學測倒數',
-                                        uri='https://www.instagram.com/ar/1507952719375055'
-                                    ),
-                                    URIAction(
-                                        label='學測倒數',
-                                        uri='https://www.instagram.com/ar/1507952719375055'
-                                    ),
-                                    URIAction(
-                                        label='學測倒數',
-                                        uri='https://www.instagram.com/ar/1507952719375055'
-                                    ),
-                                ]
-                            )
+                    event.reply_token,
+                    TemplateSendMessage(
+                        alt_text='Buttons template',
+                        template=ButtonsTemplate(
+                            title='瑋彥的作品集',
+                            text='請選擇作品',
+                            actions=[
+                                MessageTemplateAction(
+                                    label='IG濾鏡',
+                                    uri=https://www.instagram.com/ar/1507952719375055/
+                                ),
+                                MessageTemplateAction(
+                                    label='youtube',
+                                    text='youtubeㄛ'
+                                ),
+                                MessageTemplateAction(
+                                    label='網頁',
+                                    text='網頁'
+                                )
+                            ]
                         )
                     )
+                )
                     
     line_bot_api.reply_message(
         event.reply_token,
