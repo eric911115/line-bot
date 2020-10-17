3df990efc177a6f5e988a0829ea63f60cf6e5e13
eric wei <aeric911115@gmail.com>
2020-10-17 21:06:02 +0800

    add menu

--- app.py
+++ app.py
@@ -47,6 +47,18 @@ def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=r))
+    
+    rich_menu_to_create = RichMenu(
+    size=RichMenuSize(width=2500, height=843),
+    selected=False,
+    name="Nice richmenu",
+    chat_bar_text="Tap here",
+    areas=[RichMenuArea(
+        bounds=RichMenuBounds(x=0, y=0, width=2500, height=843),
+        action=URIAction(label='Go to line.me', uri='https://line.me'))]
+)
+rich_menu_id = line_bot_api.create_rich_menu(rich_menu=rich_menu_to_create)
+print(rich_menu_id)
 
 
 if __name__ == "__main__":
