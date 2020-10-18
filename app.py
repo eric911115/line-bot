9c17eb24dcae6e0f6b14e8155df14ccbc6f7d0e2
eric wei <aeric911115@gmail.com>
2020-10-19 01:04:38 +0800

    upload

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ def handle_message(event):
                             ]
                         )
                     )
-                        line_bot_api.reply_message(event.reply_token,Image_Carousel)
+                        line_bot_api.reply_message(event.reply_token,image_carousel_template_message)
                  
 
                     
