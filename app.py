a12597f2e89f8fc26cb3d274664f4d55b7bcad88
eric wei <aeric911115@gmail.com>
2020-10-18 15:40:58 +0800

    upload

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ def handle_message(event):
                     template=ButtonsTemplate(
                         title='這是ButtonsTemplate',
                         text='ButtonsTemplate可以傳送text,uri',
-                        thumbnail_image_url='顯示在開頭的大圖片網址',
+                        thumbnail_image_url='https://i.ibb.co/jfJpM2W/S-33800212.jpg',
                         actions=[
                             MessageTemplateAction(
                                 label='ButtonsTemplate',
