6836b8b6a7877892e2cd2464d0559fca34acbd6e
eric wei <aeric911115@gmail.com>
2020-10-18 21:51:20 +0800

    upload

--- app.py
+++ app.py
@@ -48,32 +48,7 @@ def handle_message(event):
         r = '你好'
     elif msg == '為什麼要瑋彥':
         r = '太可愛'
-    elif msg == "關於瑋彥":
-                line_bot_api.reply_message(  # 回復傳入的訊息文字
-                    event.reply_token,
-                    TemplateSendMessage(
-                        alt_text='Buttons template',
-                        template=ButtonsTemplate(
-                            title='瑋彥的作品集',
-                            text='請選擇作品',
-                            actions=[
-                                MessageTemplateAction(
-                                    label='IG濾鏡',
-                                    text='IG濾鏡'
-                                ),
-                                MessageTemplateAction(
-                                    label='youtube',
-                                    text='youtube'
-                                ),
-                                MessageTemplateAction(
-                                    label='網頁',
-                                    text='網頁'
-                                )
-                            ]
-                        )
-                    )
-                )  
-    return msg
+     
 def Buttons_message():
     message = TemplateSendMessage(
         alt_text = 'IG濾鏡'
@@ -85,7 +60,12 @@ def Buttons_message():
                 MessageTemplateAction(
                     label = "統測倒數"
                     text = "水喔"
+                ),
+                MessageTemplateAction(
+                    label = "企鵝"
+                    text = "北極沒有企鵝"
                 )
+        
             ]
         )
     )
